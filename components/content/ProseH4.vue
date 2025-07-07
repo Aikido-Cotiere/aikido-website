e32994dfb26b4ccf16a083840f880dc6709459de
Christophe PETITJEAN <christophe@famille-petitjean.com>
2025-07-08 01:01:19 +0200

    contenu + build fix

---
+++ components/content/ProseH4.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :id="props.id" class="text-subtitle-2 my-2 font-weight-bold">
+    <a v-if="generate" :href="`#${props.id}`">
+      <slot />
+    </a>
+    <slot v-else />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from '#imports';
+
+const props = defineProps<{ id?: string }>();
+
+const { headings } = useRuntimeConfig().public.mdc;
+const generate = computed(
+  () => props.id && ((typeof headings?.anchorLinks === 'boolean' && headings?.anchorLinks === true) || (typeof headings?.anchorLinks === 'object' && headings?.anchorLinks?.h1))
+);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 
-  ssr: false,
-
   build: {
     transpile: ['vuetify'],
   },
